c37ba3cb6683d76a1be529fd7ada14ee42fda546
Gus Caplan <me@gus.host>
2017-04-10 03:21:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var Promise = require('native-or-bluebird');
+try {
+  var Promise = require('bluebird');
+} catch (_) {
+  var Promise = global.Promise;
+}
 
 module.exports = function (ms) {
   return new Promise(function (resolve) {
